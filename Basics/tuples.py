656c10610956474599b3701eef802393c47a2a02
Thorne-Musau <thornemusau@gmail.com>
2023-05-04 20:03:13 +0300

    Tuple commit

---
+++ Basics/tuples.py
@@ -0,0 +1,13 @@
+#There are different types of python data structures
+# They include 
+#:- Tuples.-- They are defined in parenthesis
+tuple1=("disco",10,1.2)
+type(tuple1)
+print(tuple1[0]) #indexing in tuples, nested indexing 
+print(tuple1[0:3]) #slicing operations in tuples
+
+#sorting 
+ratings=(0,9,5,3,1,5,4)
+newr= sorted(ratings)
+print(newr)
+
