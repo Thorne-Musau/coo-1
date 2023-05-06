4888c2a038dbfc5aaf36d476e7749c088e4d3425
Thorne-Musau <thornemusau@gmail.com>
2023-05-06 14:54:06 +0300

    while loop

---
+++ Basics/while-loop.py
@@ -0,0 +1,12 @@
+years = [1982, 1980, 1973, 2000]
+
+i = 0
+year = years[0]
+
+while(year != 1973):    
+    print(year)
+    i = i + 1
+    year = years[i]
+    
+
+print("It took ", i ,"repetitions to get out of loop.")
