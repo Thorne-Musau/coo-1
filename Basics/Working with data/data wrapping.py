d0a5d1211015f7ebdd6b2b02b66a7c715a3af930
Thorne-Musau <thornemusau@gmail.com>
2023-05-24 15:20:01 +0300

    Wrapping changes

--- Basics/Working with data/data wrapping.py
+++ Basics/Working with data/data wrapping.py
@@ -23,11 +23,41 @@ for column in missing_data.columns.values.tolist():
     print(column, missing_data[column].value_counts())
     print("")
 
-#Replacing
+#DEaling with missing data
 avg_norm_loss = df["normalized-losses"].astype("float").mean(axis=0)
 print("Average of normalized-losses:", avg_norm_loss)
 df["normalized-losses"].replace(np.nan, avg_norm_loss, inplace=True)
 
 avg_bore=df['bore'].astype('float').mean(axis=0)
 print("Average of bore:", avg_bore)
+
 df["bore"].replace(np.nan, avg_bore, inplace=True)
+df[["bore", "stroke"]] = df[["bore", "stroke"]].astype("float")
+df[["normalized-losses"]] = df[["normalized-losses"]].astype("int")
+df[["price"]] = df[["price"]].astype("float")
+df[["peak-rpm"]] = df[["peak-rpm"]].astype("float")
+
+# Data Standardization
+# Convert mpg to L/100km by mathematical operation (235 divided by mpg)
+df['city-L/100km'] = 235/df["city-mpg"]
+
+# check your transformed data 
+df.head()
+
+#Data Normalization
+df['length'] = df['length']/df['length'].max()
+df['width'] = df['width']/df['width'].max()
+
+
+#Binning
+# Binning is a process of transforming continuous numerical variables into discrete categorical 'bins' for grouped analysis.
+# df["horsepower"]=df["horsepower"].astype(int, copy=True)
+plt.plot.hist (df['horsepower'])
+
+#set x/y labels and plot
+plt.plot.xlabel("horsepower")
+plt.plot.ylabel("count")
+plt.plot.title("horsepower bins")
+
+bins = np.linspace(min(df["horsepower"]), max(df["horsepower"]), 4)
+bins
