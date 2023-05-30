95779f6da64af4691ab33951dc7a0d29d9b53783
Thorne-Musau <thornemusau@gmail.com>
2023-05-30 15:36:11 +0300

    GroupBy commit

--- Basics/Working with data/GroupBy.py
+++ Basics/Working with data/GroupBy.py
@@ -6,3 +6,9 @@ import matplotlib.pyplot as plt
 filename="/home/thorne/Desktop/new.csv"
 df = pd.read_csv(filename)
 print(df)
+
+# GroupBy- Groups data into subsets according to diff categories
+# Can bedone by grouping a single or multiple var
+
+df_test= df[["drive-wheels", "body-style", "price"]]
+df_grp= df_test.groupby(["drive-wheels", "body-style"], as_index=False).mean()
